cb3f36e56afda0256c5ca79ae7e7835d75b00953
JaeyoonCheon <cjswodbs50@gmail.com>
2023-09-09 03:54:11 +0900

    FIX: 패키지 정리
    1.  dependency와 devDependency 목록에 혼재된 패키지 정확하게 위치되도록 변경
    2.  .env 파일 로드 활성화

---
+++ .env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+  export const SOURCE_ADDRESS: string;
+  export const SOURCE_PORT: number;
+}
---
+++ src/api/client.ts
@@ -0,0 +1,12 @@
+import axios from 'axios/index';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
+
+import {SOURCE_ADDRESS, SOURCE_PORT} from '@env';
+
+// wsl2 서버 실행 시 wsl2의 IP address를 대응 후 adb 포트 연동 필요
+const baseURL = `http://172.27.134.143:${SOURCE_PORT}`;
+
+const client = axios.create({
+  baseURL,
+});
